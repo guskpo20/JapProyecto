819ac936e26e5e00f3ddc6abc53d766387408d09
Gustavo <75509324+guskpo20@users.noreply.github.com>
2022-08-16 19:57:34 -0300

    Console.log de los datos de google

--- js/login.js
+++ js/login.js
@@ -47,12 +47,12 @@ form.addEventListener('submit', (e) => {
 
 function handleCredentialResponse(response) {
   const responsePayload = decodeJwtResponse(response.credential);
-  /*console.log('ID: ' + responsePayload.sub);
+  console.log('ID: ' + responsePayload.sub);
   console.log('Full Name: ' + responsePayload.name);
   console.log('Given Name: ' + responsePayload.given_name);
   console.log('Family Name: ' + responsePayload.family_name);
   console.log('Image URL: ' + responsePayload.picture);
-  console.log('Email: ' + responsePayload.email);*/
+  console.log('Email: ' + responsePayload.email);
   window.location.href = '/JapProyecto/home.html';
 }
 
