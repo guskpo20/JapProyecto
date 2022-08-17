7f69ed1f43b8b589ba0eaec9f3d8bbe156a5b25d
Gustavo <75509324+guskpo20@users.noreply.github.com>
2022-08-17 17:47:18 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -53,9 +53,6 @@ function handleCredentialResponse(response) {
   console.log('Family Name: ' + responsePayload.family_name);
   console.log('Image URL: ' + responsePayload.picture);
   console.log('Email: ' + responsePayload.email);
-  prompt(
-    'Hola ' + responsePayload.name + ', su correo es ' + responsePayload.email
-  );
   window.location.href = '/JapProyecto/home.html';
 }
 
